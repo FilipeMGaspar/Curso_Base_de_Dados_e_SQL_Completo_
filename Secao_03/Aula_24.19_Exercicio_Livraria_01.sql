14889f22109a0d61d7fbc5f36cdff9f14fd8ab68
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-04 11:21:44 +0100

    Aula 24. 19 Exercício Livraria

--- Secao_03/Aula_24.19_Exercicio_Livraria_01.sql
+++ Secao_03/Aula_24.19_Exercicio_Livraria_01.sql
@@ -18,3 +18,17 @@ USE LIVRARIA;
  );
  
  select * from LIVROS;
+ 
+/* INSERINDO DADOS NA TABELA */
+ INSERT INTO LIVROS (NOME_DO_LIVRO, NOME_DO_AUTOR, SEXO_DO_AUTOR, NUMERO_DE_PAGINAS,
+ NOME_DA_EDITORA, VALOR_DO_LIVRO, UF_DA_EDITORA, ANO_PUBLICACAO) VALUES
+ ('Cavaleiro Real','Ana Claudia','FEMININO',465,'Atlas',49.9,'RJ',2009),
+ ('SQL para leigos','João Nunes','MASCULINO',450,'Addison',98,'SP',2018),
+ ('Receitas Caseiras','Celia Tavares','FEMININO',210,'Atlas',45,'RJ',2008),
+ ('Pessoas Efetivas','Eduardo Santos','MASCULINO',390,'Beta',78.99,'RJ',2018),
+ ('Habitos Saudáveis','Eduardo Santos','MASCULINO',630,'Beta',150.98,'RJ',2019),
+ ('A Casa Marrom','Hermes Macedo','MASCULINO',250,'Bubba',60,'MG',2016),
+ ('Estacio Querido','Geraldo Francisco','MASCULINO',310,'Insignia',100,'ES',2015),
+ ('Pra sempre amigas','Leda Silva','FEMININO',510,'Insignia',78.98,'ES',2011),
+ ('Copas Inesqueciveis','Marco Alcantara','MASCULINO',200,'Larson',130.98,'RS',2018),
+ ('O poder da mente','Clara Mafra','FEMININO',120,'Continital',56.58,'RS',2017);
